0738d1387d365d4a78ec0e07c5f90c1a1944bd4c
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2020-07-21 22:07:21 -0400

    added pwd command

--- bash.js
+++ bash.js
@@ -1,8 +1,14 @@
-process.stdout.write('prompt >');
+//let fs = require('fs');
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
+process.stdout.write('prompt >'); // output of 'prompt >'
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+process.stdin.on('data', (data) => { // input of something (here called data)
+  const cmd = data.toString().trim(); // convert that data to string and store in cmd
+
+  if (cmd === 'pwd') { // if input (cmd) was pwd
+    console.log(process.cwd()) // print current working directory
+  }
+
+  process.stdout.write('You typed: ' + cmd); // output 'you typed' + inputted data
+  process.stdout.write('\nprompt > '); // create new line and write 'prompt >' again
 });
